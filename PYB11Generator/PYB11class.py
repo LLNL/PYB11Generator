ecb84094aea4c2a3a9abf04ff95dc30adda30624
Brody Richard Bassett <bassett4@llnl.gov>
2019-03-19 10:07:01 -0700

    Added back in commented lines.

--- PYB11Generator/PYB11class.py
+++ PYB11Generator/PYB11class.py
@@ -411,8 +411,8 @@ def PYB11generateClass(klass, klassattrs, ssout):
     cppname = "%(namespace)s%(cppname)s" % klassattrs
     bklasses = PYB11getBaseClasses(klass)
     Tdict = PYB11parseTemplates(klassattrs, bklasses)
-    # for key in klassattrs["template_dict"]:
-    #     Tdict[key] = klassattrs["template_dict"][key]
+    for key in klassattrs["template_dict"]:
+        Tdict[key] = klassattrs["template_dict"][key]
     for bklass in bklasses[klass]:
         bklassattrs = PYB11attrs(bklass)
         bcppname = "%(namespace)s%(cppname)s" % bklassattrs 
