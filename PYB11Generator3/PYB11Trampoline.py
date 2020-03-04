b2d36d88d0429e8b8137709339962b80e0aeeb1b
Mike Owen <mikeowen@llnl.gov>
2020-03-03 22:46:42 -0800

    Initial whack at conversion

---
+++ PYB11Generator3/PYB11Trampoline.py
@@ -0,0 +1,252 @@
+#-------------------------------------------------------------------------------
+# PYB11Trampoline
+#-------------------------------------------------------------------------------
+import inspect
+import sys
+import io
+
+from .PYB11utils import *
+
+#-------------------------------------------------------------------------------
+# PYB11generateModuleTrampolines
+#
+# Generate trampolines for any classes with virtual methods.
+#-------------------------------------------------------------------------------
+def PYB11generateModuleTrampolines(modobj, ss):
+    klasses = PYB11classes(modobj)
+
+    # Cull to just classes with virtual methods.
+    klasses = [(name, klass) for (name, klass) in klasses if PYB11virtualClass(klass)]
+
+    # Cull for things we're ignoring
+    newklasses = []
+    known_trampolines = []
+    for name, klass in klasses:
+        klassattrs = PYB11attrs(klass)
+        template_klass = len(klassattrs["template"]) > 0
+        mods = klassattrs["module"]
+        if ((template_klass or not klassattrs["ignore"]) and                 # ignore flag (except for template class)?
+            (klassattrs["pyname"] not in known_trampolines) and              # has this trampoline been generated?
+            ((klass not in mods) or mods[klass] == modobj.PYB11modulename)): # is this class imported from another mod?
+            newklasses.append((name, klass))
+            known_trampolines.append(klassattrs["pyname"])
+    klasses = newklasses
+
+    # Generate trampolines
+    for kname, klass in klasses:
+        PYB11generateTrampoline(klass, ss)
+    return
+
+#-------------------------------------------------------------------------------
+# PYB11generateTrampoline
+#
+# Generate the trampoline class, including pure virtual hooks.
+#-------------------------------------------------------------------------------
+def PYB11generateTrampoline(klass, ssout):
+
+    klassattrs = PYB11attrs(klass)
+    template_klass = len(klassattrs["template"]) > 0
+    # if klassattrs["ignore"] and not template_klass:
+    #     return
+
+    # # This is a bit of trickery to let us use inheritance without regenerating trampolines
+    # if"__known_trampolines" not in PYB11generateTrampoline.__dict__:
+    #     PYB11generateTrampoline.__known_trampolines = []
+    # if klassattrs["pyname"] in PYB11generateTrampoline.__known_trampolines:
+    #     return
+    # PYB11generateTrampoline.__known_trampolines.append(klassattrs["pyname"])
+
+    # # We also screen out classes imported from other modules.
+    # mods = klassattrs["module"]
+    # if klass in mods:
+    #     return
+
+    # Prepare in case there are templates lurking in here.
+    fs = io.StringIO()
+    ss = fs.write
+
+    # Build the dictionary of template substitutions.
+    Tdict = PYB11parseTemplates(klassattrs, inspect.getmro(klass))
+    
+    # Compiler guard.
+    ss("""//------------------------------------------------------------------------------
+// Trampoline class for %(cppname)s
+//------------------------------------------------------------------------------
+#ifndef __trampoline_%(pyname)s__
+#define __trampoline_%(pyname)s__
+
+""" % klassattrs)
+
+    # Namespaces
+    for ns in klassattrs["namespace"].split("::")[:-1]:
+        ss("namespace " + ns + " {\n")
+
+    if template_klass:
+        ss("template<")
+        for i, name in enumerate(klassattrs["template"]):
+            if i < len(klassattrs["template"]) - 1:
+                ss("%s, " % name)
+            else:
+                ss("%s>\n" % name)
+
+    # Build the base class hierarchy names
+    bklassnames = []
+    for bklass in inspect.getmro(klass):
+        bklassattrs = PYB11attrs(bklass)
+        bklassname = "%(namespace)s%(cppname)s" % bklassattrs
+        if len(bklassattrs["template"]) > 0:
+            bklassname += "<"
+            for i, name in enumerate(bklassattrs["template"]):
+                assert len(name.split()) == 2
+                nameval = name.split()[1]
+                if name in klassattrs["template"]:
+                    bklassname += nameval
+                else:
+                    if not nameval in Tdict:
+                        raise RuntimeError("Trampoline template base class error: %s is missing from specified template parameters %s\n  (class, base) = (%s, %s)" % (nameval, Tdict, klass, bklass))
+                    bklassname += Tdict[nameval]
+                if i < len(bklassattrs["template"]) - 1:
+                    bklassname += ", "
+            bklassname += ">"
+        bklassnames.append(bklassname)
+    assert len(bklassnames) == len(inspect.getmro(klass))
+    bklassnames[0] = "PYB11self"
+
+    # Class name
+    ss("""class PYB11Trampoline%(cppname)s: public %(full_cppname)s {
+public:
+  using %(full_cppname)s::%(cppname)s;   // inherit constructors
+  typedef %(full_cppname)s PYB11self;    // Necessary to protect macros below from names with commas in them
+""" % klassattrs)
+    for bklassname in bklassnames[1:]:
+        if bklassname != PYB11mangle(bklassname):
+            ss("  typedef %s %s;\n" % (bklassname, PYB11mangle(bklassname)))
+
+    # # Use any nested class definitions
+    # klasses = [(x, eval("klass.%s" % x)) for x in dir(klass) if (inspect.isclass(eval("klass.%s" % x)) and x in klass.__dict__)]
+    # for (kname, nklass) in klasses:
+    #     nklassattrs = PYB11attrs(nklass)
+    #     ss("  typedef typename %(full_cppname)s::" % klassattrs)
+    #     ss("%(cppname)s %(cppname)s;\n" % nklassattrs)
+
+    # Any typedefs?
+    if hasattr(klass, "PYB11typedefs"):
+        typedefs = str(klass.PYB11typedefs)
+    else:
+        typedefs = ""
+
+    # Bind the (unique) virtual methods for all classes up the inheritance tree.
+    # We use an independent StringIO object for this, since we may have some new typedefs that
+    # need to be added before this stuff is output to the source.
+    methfms = io.StringIO()
+    boundMethods = []
+    for (bklass, bklassname) in zip(inspect.getmro(klass), bklassnames):
+
+        bklassinst = bklass()
+        bklassattrs = PYB11attrs(bklass)
+        methods = [(mname, meth) for (mname, meth) in PYB11ClassMethods(bklass)
+                   if (not PYB11attrs(meth)["ignore"] and
+                       (PYB11attrs(meth)["virtual"] or PYB11attrs(meth)["pure_virtual"]) and
+                       mname in bklass.__dict__)]
+
+        # Look for any template parameters of the base not shared by the class in question
+        bklasssubs = {}
+        for name in bklassattrs["template"]:
+            if not name in klassattrs["template"]:
+                nameval = name.split()[1]
+                assert nameval in Tdict
+                bklasssubs[name] = Tdict[nameval]
+
+        for mname, meth in methods:
+            
+            # We build this method string up independent of the output stream
+            # until we determine if it's already been generated.
+            fms = io.StringIO()
+
+            methattrs = PYB11attrs(meth)
+            methattrs["returnType"] = eval("bklassinst." + mname + "()")
+            assert methattrs["returnType"]    # We require the full spec for virtual methods
+            fms.write("  virtual %(returnType)s %(cppname)s(" % methattrs)
+
+            # Fill out the argument list for this method
+            args = PYB11parseArgs(meth)
+            for i, (argType, argName, default) in enumerate(args):
+                fms.write("%s %s" % (argType, argName))
+                if i < len(args) - 1:
+                    fms.write(", ")
+            if methattrs["const"]:
+                fms.write(") const override { ")
+            else:
+                fms.write(") override { ")
+
+            # At this point we can make the call of whether this is a new method.
+            try:
+                thpt = fms.getvalue() % Tdict
+            except:
+                raise RuntimeError("Unable to generate call descriptor for %s in %s->%s" % (mname, str(klass), bklassname))
+            if not thpt in boundMethods:
+                boundMethods.append(fms.getvalue() % Tdict)
+
+                # Check if the returnType C++ name will choke PYBIND11_OVERLOAD*
+                returnType = methattrs["returnType"]
+                if PYB11badchars(returnType):
+                    returnType = PYB11mangle(returnType)
+                    typedefstring = "    typedef %s %s;\n" % (methattrs["returnType"], returnType)
+                    if typedefstring not in typedefs:
+                        typedefs += typedefstring
+                    methattrs["returnType"] = returnType
+
+                # Check if we need to work around any reference arguments due to the pybind11 bug discussed in
+                # https://stackoverflow.com/questions/59330279/problems-passing-a-stdvector-by-reference-though-virtual-functions-using-pybin/59331026?noredirect=1#comment104861677_59331026
+                altered = False
+                for i, (argType, argName, default) in enumerate(args):
+                    if "&" in argType:
+                        altered = True
+                        fms.write("\n    py::object dummy%i = py::cast(&%s);\n" % (i, argName))
+                if altered:
+                    fms.write("    ")
+
+                if methattrs["pure_virtual"]:
+                    fms.write("PYBIND11_OVERLOAD_PURE(%(returnType)s, PYB11self, %(cppname)s, " % methattrs)
+                else:
+                    # HACK!  To workaround what appears to be a bug in overloading virtual method callbacks
+                    # in pybind11 (see https://github.com/pybind/pybind11/issues/1547), we have to give
+                    # the address of the object that actually implements it.  This is clealy not how a human
+                    # should have to handle this, but since we're code generating this we can do this explicit
+                    # workaround.
+                    #fms.write("PYBIND11_OVERLOAD(%(returnType)s, PYB11self, %(cppname)s, " % methattrs)
+                    fms.write("PYBIND11_OVERLOAD(%(returnType)s, " % methattrs)
+                    fms.write(PYB11mangle(bklassname) + ", ")
+                    fms.write("%(cppname)s, " % methattrs)
+
+                for i, (argType, argName, default) in enumerate(args):
+                    if i < len(args) - 1:
+                        fms.write(argName + ", ")
+                    else:
+                        fms.write(argName)
+                if altered:
+                    fms.write(");\n  }\n")
+                else:
+                    fms.write("); }\n")
+
+                # Write to the method overloading stream.
+                methfms.write(fms.getvalue())
+            fms.close()
+
+    # Write the full typdefs
+    ss(typedefs + "\n")
+
+    # Write the method overloads
+    ss(methfms.getvalue())
+    methfms.close()
+
+    # Closing
+    ss("};\n\n")
+    for ns in klassattrs["namespace"].split("::")[:-1]:
+        ss("}\n")
+    ss("\n#endif\n")
+
+    # Sub any template parameters.
+    ssout(fs.getvalue() % Tdict)
+
+    return
---
+++ PYB11Generator3/PYB11function.py
@@ -0,0 +1,189 @@
+#-------------------------------------------------------------------------------
+# PYB11function
+#
+# Handle functions in pybind11
+#-------------------------------------------------------------------------------
+from .PYB11utils import *
+from .PYB11property import *
+import copy, io
+
+#-------------------------------------------------------------------------------
+# PYB11generateModuleFunctions
+#
+# Bind the methods in the module
+#-------------------------------------------------------------------------------
+def PYB11generateModuleFunctions(modobj, ss):
+    methods = PYB11functions(modobj)
+    if methods:
+        ss("  //...........................................................................\n")
+        ss("  // Methods\n")
+        for name, meth in methods:
+            methattrs = PYB11attrs(meth)
+            if not methattrs["ignore"]:
+                PYB11generateFunction(meth, methattrs, ss)
+
+    # Now look for any template function instantiations.
+    func_templates = [x for x in dir(modobj) if isinstance(eval("modobj.%s" % x), PYB11TemplateFunction)]
+    for ftname in func_templates:
+        func_template = eval("modobj.%s" % ftname)
+        func_template(ftname, ss)
+    return
+
+#--------------------------------------------------------------------------------
+# Make a function template instantiation
+#--------------------------------------------------------------------------------
+class PYB11TemplateFunction:
+
+    def __init__(self,
+                 func_template,
+                 template_parameters,
+                 cppname = None,
+                 pyname = None,
+                 docext = ""):
+        self.func_template = func_template
+        self.cppname = cppname
+        self.pyname = pyname
+        self.docext = docext
+
+        # Create the template parameter dictionary
+        self.template_parameters = {}
+        funcattrs = PYB11attrs(self.func_template)
+        if isinstance(template_parameters, str):
+            assert len(funcattrs["template"]) == 1
+            self.template_parameters[funcattrs["template"][0].split()[1]] = template_parameters
+        elif isinstance(template_parameters, tuple):
+            assert len(funcattrs["template"]) == len(template_parameters)
+            for name, val in zip(funcattrs["template"], template_parameters):
+                self.template_parameters[name.split()[1]] = val
+        else:
+            assert isinstance(template_parameters, dict)
+            for arg in funcattrs["template"]:
+                key = arg.split()[1]
+                if not key in template_parameters:
+                    raise RuntimeError("Template parameter dictionary spec error: %s is missing from %s" % (key, template_parameters))
+            self.template_parameters = template_parameters
+            
+        # Check for any explicit template dictionaries
+        if funcattrs["template_dict"]:
+            for key in funcattrs["template_dict"]:
+                if not key in self.template_parameters:
+                    self.template_parameters[key] = funcattrs["template_dict"][key]
+
+        self.template_parameters = PYB11recurseTemplateDict(self.template_parameters)
+        return
+
+    def __call__(self, pyname, ss):
+        funcattrs = PYB11attrs(self.func_template)
+
+        # Do some template mangling (and magically put the template parameters in scope).
+        template_ext = "<"
+        doc_ext = ""
+        for name in funcattrs["template"]:
+            name = name.split()[1]
+            val = self.template_parameters[name]
+            exec("%s = '%s'" % (name, val))
+            template_ext += "%s, " % val
+            doc_ext += "_%s_" % val.replace("::", "_").replace("<", "_").replace(">", "_")
+        template_ext = template_ext[:-2] + ">"
+
+        if self.cppname:
+            funcattrs["cppname"] = self.cppname
+        else:
+            funcattrs["cppname"] += template_ext
+        if self.pyname:
+            funcattrs["pyname"] = self.pyname
+        else:
+            funcattrs["pyname"] = pyname
+
+        funcattrs["template_dict"] = {}
+        for name, val in self.template_parameters.items():
+            funcattrs["template_dict"][name] = val
+
+        if self.func_template.__doc__:
+            doc0 = copy.deepcopy(self.func_template.__doc__)
+            self.func_template.__doc__ += self.docext
+        PYB11generateFunction(self.func_template, funcattrs, ss)
+        if self.func_template.__doc__:
+            self.func_template.__doc__ = doc0
+        return
+
+#-------------------------------------------------------------------------------
+# Generate a function definition
+#-------------------------------------------------------------------------------
+def PYB11generateFunction(meth, methattrs, ssout):
+    fs = io.StringIO()
+    ss = fs.write
+
+    # Arguments
+    stuff = inspect.getargspec(meth)
+    nargs = len(stuff.args)
+    argNames = stuff.args
+    argTypes, argDefaults = [], []
+    if nargs > 0:
+        for thing in stuff.defaults:
+            if isinstance(thing, tuple):
+                assert len(thing) == 2
+                argTypes.append(thing[0])
+                argDefaults.append(thing[1])
+            else:
+                argTypes.append(thing)
+                argDefaults.append(None)
+    assert len(argNames) == nargs
+    assert len(argTypes) == nargs
+    assert len(argDefaults) == nargs
+
+    # Return type
+    returnType = meth(*tuple(stuff.args))
+    methattrs["returnType"] = returnType
+
+    # Build the argument string if any
+    argString = ""
+    for argType, argName, default in zip(argTypes, argNames, argDefaults):
+        argString += (', "%s"_a' % argName)
+        if methattrs["noconvert"]:
+            argString += '.noconvert()'
+        if not default is None:
+            argString += ("=" + default)
+
+    # Write the binding
+    ss('  m.def("%(pyname)s", ' % methattrs)
+
+    # If there is an implementation, short-circuit the rest of our work.
+    if methattrs["implementation"]:
+        ss(methattrs["implementation"] + argString)
+
+    elif returnType:
+        ss("(%s (*)(" % returnType)
+        for i, argType in enumerate(argTypes):
+            ss(argType)
+            if i < nargs - 1:
+                ss(", ")
+        ss(")) &%(namespace)s%(cppname)s" % methattrs + argString)
+    else:
+        ss("&%(namespace)s%(cppname)s" % methattrs)
+
+    # Is there a return value policy?
+    if methattrs["returnpolicy"]:
+        ss(", py::return_value_policy::%s" % methattrs["returnpolicy"])
+
+    # Is there a call guard?
+    if methattrs["call_guard"]:
+        ss(", py::call_guard<%s>()" % methattrs["call_guard"])
+
+    # Is there a keep_alive policy?
+    if methattrs["keepalive"]:
+        assert isinstance(methattrs["keepalive"], tuple)
+        assert len(methattrs["keepalive"]) == 2
+        ss(", py::keep_alive<%i, %i>()" % methattrs["keepalive"])
+
+    # Write the doc string
+    doc = inspect.getdoc(meth)
+    if doc:
+        ss(", ")
+        PYB11docstring(doc, ss)
+    ss(");\n")
+
+    ssout(fs.getvalue() % methattrs["template_dict"])
+    fs.close()
+    return
+
