4754dd178268b9db85de77c39549e3c5e643ded7
Mike Owen <mikeowen@llnl.gov>
2020-03-03 22:16:08 -0800

    Current version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PYB11Generator",
-    version="1.0.8",
+    version="1.0.9",
     author="J. Michael Owen",
     author_email="mikeowen@llnl.gov",
     description="A code generator for the pybind11 C++ <-> Python language binding tool",
