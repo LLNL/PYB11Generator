721f24459ffd200b767306dda1bc67fb0eb19465
Mike Owen <mikeowen@llnl.gov>
2022-01-11 14:46:22 -0800

    Some spiffing up with the Python3 port

--- cmake/moduleCheck.py
+++ cmake/moduleCheck.py
@@ -9,21 +9,21 @@ mod_file = sys.argv[2]
 
 finder = ModuleFinder()
 finder.run_script(mod_file)
+#print("Warning failed imports: ", finder.badmodules.keys())
 
 current_stamp_name = mod_name + "_stamp.cmake"
 tmp_stamp_name = current_stamp_name + ".tmp"
 
-newF = open(tmp_stamp_name, "w")
-newF.write("set("+mod_name+"_DEPENDS \n")
+with open(tmp_stamp_name, "w") as newF:
+    newF.write("set("+mod_name+"_DEPENDS \n")
 
-for name, mod in finder.modules.items():
-  if (mod.__file__):
-    if not ("lib/python3" in mod.__file__):
-      newF.write(mod.__file__)
-      newF.write('\n')
+    for name, mod in finder.modules.items():
+      if (mod.__file__):
+        if not ("lib/python3" in mod.__file__):
+          newF.write(mod.__file__)
+          newF.write('\n')
 
-newF.write(")\n")
-newF.close()
+    newF.write(")\n")
 
 if (os.path.isfile(current_stamp_name)):
   if (not filecmp.cmp(current_stamp_name, tmp_stamp_name)):
