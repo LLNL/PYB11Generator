76ada0ec4f43b03d602d3cc68fd5dec3ee8ce26d
Mike Owen <mikeowen@llnl.gov>
2023-03-08 14:39:29 -0800

    Fixed the new method of generating and checking for changes with CMake. The basename parameter to PYB11Generate module cannot be a completely arbitrary name, it will wind up in the source.  At some point we should make this cleaner.

--- cmake/generate_cpp.py
+++ cmake/generate_cpp.py
@@ -1,30 +1,39 @@
 import sys
 import os
+import shutil
 import filecmp
 
 pyb11_mod_name = sys.argv[1]
 mod_name = sys.argv[2]
-tmp_name = mod_name + "_tmp"
 
+tmp_dir = "tmp_dir"
+current_src = mod_name + ".cc"
+stashed_src = os.path.join(tmp_dir, current_src)
+
+# Stash away the existing module code (if any)
+if os.path.isfile(current_src):
+    if os.path.exists(tmp_dir):
+        shutil.rmtree(tmp_dir)
+    os.mkdir(tmp_dir)
+    os.rename(current_src, stashed_src)
+assert not os.path.isfile(current_src)
+
+# Generate the source anew 
 code = """
 from PYB11Generator import *
-import ${pyb11_module}
-PYB11generateModule(${pyb11_module}, \"${tmp_name}\")
-"""
-code = code.replace("${pyb11_module}", pyb11_mod_name)
-code = code.replace("${tmp_name}", tmp_name)
-
+import {pyb11_module}
+PYB11generateModule({pyb11_module}, \"{mod_name}\")
+""".format(pyb11_module = pyb11_mod_name,
+           mod_name     = mod_name)
 exec(code)
+assert os.path.isfile(current_src)
 
-current_src = mod_name + ".cc"
-tmp_src = mod_name + "_tmp.cc"
+# If the module source is unchanged, put it back
+if os.path.isfile(stashed_src):
+    if filecmp.cmp(current_src, stashed_src):
+        os.remove(current_src)
+        os.rename(stashed_src, current_src)
 
-if (os.path.isfile(current_src)):
-  if (not filecmp.cmp(current_src, tmp_src)):
-    os.rename(tmp_src, current_src)
-  else:
-    os.remove(tmp_src)
-else:
-  os.rename(tmp_src, current_src)
-
-
+# Clean up
+if os.path.exists(tmp_dir):
+    shutil.rmtree(tmp_dir)
